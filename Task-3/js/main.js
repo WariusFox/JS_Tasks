6d1854e338d5b56a407b8805c95ba6d0ae1f7d77
WariusFox <badboy101@bk.ru>
2024-03-03 17:10:25 +0400

    Task-3

---
+++ Task-3/js/main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// Tapşırıq 1
+// let age = parseInt(prompt("Yasinizi daxil edin: "));
+
+// if (age >= 18) {
+//   alert("Avtomobil sürmək üçün kifayət qədər yaşlisiniz.");
+// } else {
+//   let yearsLeft = 18 - age;
+//   alert("Sizə maşin sürmək üçün " + yearsLeft + "il vaxt qalir.");
+// }
+
+// Tapşırıq 2
+// let age = parseInt(prompt("Yaşinizi daxil edin: "));
+// const myAge = 45;
+
+// if (age > myAge) {
+//   alert("Sən məndən " + (age - myAge) + " yaş böyüksən.");
+// } else if (age < myAge) {
+//   alert("Sən məndən " + (myAge - age) + " yaş kiçiksən.");
+// } else {
+//   alert("Biz eyni yaşdayıq.");
+// }
+
+// Tapşırıq 3
+// let number = parseInt(prompt("Nömrə daxil edin: "));
+
+// if (number % 2 === 0) {
+//   alert(number + " cüt ədəddir.");
+// } else {
+//   alert(number + " tək rəqəmdir.");
+// }
